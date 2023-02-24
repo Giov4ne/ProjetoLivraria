f419510a927ad1cd8bf2d651a30268711b4a2fa6
Giovane William Budal <giovanewbudal@gmail.com>
2023-02-24 13:01:28 -0300

    primeiro commit do projeto

---
+++ banco de dados/banco.sql
@@ -0,0 +1,42 @@
+/* CRIAÇÃO DA BASE DE DADOS */
+create database magic_world_bookstore;
+use magic_world_bookstore;
+
+/* TABELAS */
+create table usuario(
+    cpf varchar(14) primary key not null,
+    nome varchar(100) not null,
+    dt_nasc date not null,
+    email varchar(100) not null,
+    senha varchar(50) not null,
+    tipo varchar(7) not null check(tipo in ('admin','cliente'))
+);
+
+create table compra(
+    cod int primary key auto_increment,
+    dt_comp date not null,
+    hr_comp time not null,
+    valor_total decimal(10,2) not null,
+    forma_pagamento varchar(50) not null,
+    cpf_usuario varchar(14) not null,
+        foreign key (cpf_usuario) references usuario(cpf) on update cascade on delete restrict
+);
+
+create table livro(
+    cod int primary key not null,
+    titulo varchar(100) not null unique,
+    autor varchar(100) not null,
+    genero varchar(50) not null,
+    preco decimal(10,2) not null,
+    qtd_estoque int not null
+);
+
+create table comp_livro(
+    cod_compra int not null,
+    cod_livro int not null,
+        foreign key (cod_compra) references compra(cod) on update cascade on delete restrict,
+        foreign key (cod_livro) references livro(cod) on update cascade on delete restrict
+);
+
+/* ADMINISTRADOR */
+insert into usuario(cpf, nome, dt_nasc, email, senha, tipo) values('079.418.029-96', 'Giovane William Budal', '2005-03-07', 'giovanewbudal@gmail.com', 'Gi0v4ne123', 'admin');
