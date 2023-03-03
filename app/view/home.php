ecb6b9afd74324b0e62d63bebac3a177ceb4ff19
Giovane William Budal <giovanewbudal@gmail.com>
2023-03-03 13:43:17 -0300

    alteracao no prototipo do figma e criacao da pagina principal

---
+++ app/view/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/style.css">
+        <title>Magic World Bookstore</title>
+    </head>
+    <body>
+        <header>
+            <img src="../img/logo.png" alt="logo" id="logo">
+            <div id="pesquisa">
+                <input type="text" id="barra-pesquisa">
+                <div>
+                    <img src="../icons/lupa.svg" alt="lupa" id="lupa">
+                </div>
+            </div>
+            <div id="conta">
+                <img src="../icons/conta.png" alt="perfil" class="icones" id="img-perfil">
+                <div>
+                    <span>Bem-vindo(a)</span>
+                    <span>Username</span>
+                </div>
+            </div>
+            <img src="../icons/carrinho.png" alt="carrinho" class="icones" id="carrinho">
+        </header>
+    </body>
+</html>
