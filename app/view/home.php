e7285946713e8bb0b6284c4d2a3cbdc6f1ed06cf
Giovane William Budal <giovanewbudal@gmail.com>
2023-03-06 14:57:54 -0300

    construcao da pagina home 0.1

--- app/view/home.php
+++ app/view/home.php
@@ -10,19 +10,118 @@
         <header>
             <img src="../img/logo.png" alt="logo" id="logo">
             <div id="pesquisa">
-                <input type="text" id="barra-pesquisa">
-                <div>
-                    <img src="../icons/lupa.svg" alt="lupa" id="lupa">
-                </div>
+                <input type="text" name="search" id="barra-pesquisa">
+                <img src="../icons/lupa.svg" alt="lupa" class="icones" id="lupa">
             </div>
-            <div id="conta">
-                <img src="../icons/conta.png" alt="perfil" class="icones" id="img-perfil">
-                <div>
-                    <span>Bem-vindo(a)</span>
-                    <span>Username</span>
+            <div id="conta-carrinho">
+                <div id="conta">
+                    <img src="../icons/conta.svg" alt="perfil" class="icones" id="perfil">
+                    <div>
+                        <span class="boas-vindas">Bem-vindo(a)</span>
+                        <span class="boas-vindas">Username</span>
+                    </div>
                 </div>
+                <img src="../icons/carrinho.svg" alt="carrinho" class="icones" id="carrinho">
             </div>
-            <img src="../icons/carrinho.png" alt="carrinho" class="icones" id="carrinho">
         </header>
+        <section id="banner">
+            <img src="../img/banner.jpg" alt="banner" id="hero">
+        </section>
+        <section id="produtos">
+            <section id="filtros">
+                <div>
+                    <p>Filtrar por:</p>
+                    <p>Filtro</p>
+                    <p>Filtro</p>
+                    <p>Filtro</p>
+                    <p>Filtro</p>
+                    <p>Filtro</p>
+                    <p>Filtro</p>
+                    <p>Filtro</p>
+                    <p>Filtro</p>
+                    <p>Filtro</p>
+                    <p>Filtro</p>
+                </div>
+            </section>
+            <section id="catalogo">
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+                <p>Produto</p>
+            </section>
+        </section>
+        <button id="voltar" onclick="window.scroll({top: 0, behavior: 'smooth'})">Voltar ao início</button>
+        <footer>Magic World Bookstore © 2023 - Todos os direitos reservados.</footer>
     </body>
 </html>
